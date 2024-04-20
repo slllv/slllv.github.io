b13599a25304bec6a4d1aaf3439ef4b3d9e6d6f1
Claudio N'danu <62394444+slllv@users.noreply.github.com>
2024-04-20 21:51:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,23 +7,6 @@ function addBloon() {
     var bloon = document.createElement('div');
     bloon.classList.add('bloon');
 
-    // Firebase database reference
-    var database = firebase.database();
-    var bloonsRef = database.ref('bloons');
-
-    // Create a new bloon object
-    var newBloon = {
-        text: bloonText,
-        createdAt: firebase.database.ServerValue.TIMESTAMP
-    };
-
-    // Push the new bloon to the database
-    bloonsRef.push(newBloon)
-        .then(function() {
-            console.log("Bloon added successfully!");
-            // Clear input field after adding bloon
-            document.getElementById('messageInput').value = '';
-
             // Set the bloon text with formatted message
             var formattedMessage = bloonText.replace(/(\:\)|\:-\)|\:\(|\:-\(|\:\D|<3)/g, '<span style="color: red;">$&</span>');
             bloon.innerHTML = formattedMessage;
